5c17d6d8670f321df17d4b095c8a1722539e82e7
Kyle Burton <kburton@relaynetwork.com>
2010-05-10 18:10:21 -0400

    symlink the xpath utility, export path after profile init

--- users/kburton/.bashrc
+++ users/kburton/.bashrc
@@ -1,3 +1,4 @@
 export PATH="$PATH:$HOME/local/homebrew/bin:$HOME/local/homebrew/sbin"
 . ~/.profile.d/init
 
+export PATH
--- users/kburton/install.sh
+++ users/kburton/install.sh
@@ -2,4 +2,4 @@ set -e
 set -x
 
 test -d $HOME/bin || mkdir $HOME/bin
-test -f $HOME/bin/xpath || ln -s $(dirname $0)/bin/xpath $HOME/bin/xpath
+test -f $HOME/bin/xpath || ln -s $HOME/.profile.d/users/kburton/bin/xpath $HOME/bin/xpath
