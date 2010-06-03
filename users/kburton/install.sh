78a1a5875f0b896578998eee6252ad1d480e8050
Kyle Burton <kburton@relaynetwork.com>
2010-06-03 10:08:17 -0400

    order preserving unique

--- users/kburton/install.sh
+++ users/kburton/install.sh
@@ -2,6 +2,6 @@ set -e
 set -x
 
 test -d $HOME/bin || mkdir $HOME/bin
-for util in xpath rec-view rabbit justify; do
+for util in xpath rec-view rabbit justify runiq; do
   test -f $HOME/bin/$util || ln -s $HOME/.profile.d/users/kburton/bin/$util $HOME/bin/$util
 done
