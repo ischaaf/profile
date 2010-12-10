c76319af21b96aa41ea5ac4a9b03ba6ee0c5c36f
Kyle Burton <kburton@relaynetwork.com>
2010-12-10 09:49:29 -0500

    created relay user, adaptations for relay

--- scripts/mvn-completion.sh
+++ scripts/mvn-completion.sh
@@ -25,7 +25,7 @@ _m2_complete()
   goals=$goals _m2_make_goals "javadoc" "javadoc"
   goals=$goals _m2_make_goals "plexus" "app bundle-application bundle-runtime descriptor runtime service"
   goals=$goals _m2_make_goals "clojure" "compile test run repl swank nailgun"
-  goals=$goals _m2_make_goals "dependency" "tree classpath"
+  goals=$goals _m2_make_goals "dependency" "tree build-classpath"
 
   cur=`echo $cur | sed 's/\\\\//g'`
   COMPREPLY=($(compgen -W "${goals}" ${cur} | sed 's/\\\\//g') )
