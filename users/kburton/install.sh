d7b8e234709f92208230d5c5b6a10395dbe231d1
Kyle Burton <kyle.burton@gmail.com>
2016-05-22 11:05:34 -0700

    util for managing multiple ssh environments

--- users/kburton/install.sh
+++ users/kburton/install.sh
@@ -11,3 +11,14 @@ if [ ! -d $HOME/.vim ]; then
   test -d $HOME/.vim/backup      || mkdir $HOME/.vim/backup
   test -d $HOME/.vim/backup/swap || mkdir $HOME/.vim/backup/swap
 fi
+
+
+if [ ! -d $HOME/.bash.d ]; then
+  mkdir -d $HOME/.bash.d
+fi
+for f in $HOME/.profile.d/users/kburton/.bash.d/*; do
+  fname="$(basename $f)"
+  if [ ! -e $HOME/.bash.d/$fname ]; then
+    ln -s "$f" "$HOME/.bash.d/$fname"
+  fi
+done
