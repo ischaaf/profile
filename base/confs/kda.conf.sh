1d0e4444703afdaa67cf64d6ef9a68d819add60d
Isaac Schaaf <ischaaf@riotgames.com>
2018-04-30 17:07:45 -0700

    updated local conf for rito, updated bad java conf

---
+++ base/confs/kda.conf.sh
@@ -0,0 +1,3 @@
+export PATH=$R_CODE/egh.riotgames.com/kda/tools-kda/tools/bin:$PATH
+export BAKEPATH=$R_CODE/egh.riotgames.com/kda/tools-kda/bake
+for f in $R_CODE/egh.riotgames.com/kda/tools-kda/tools/sourced/*; do source "$f"; done
