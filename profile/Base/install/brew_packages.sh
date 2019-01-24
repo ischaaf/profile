bfcb1ad5ba0de0d9347f27d2af9840fdaf6a36b4
Isaac Schaaf <ischaaf@riotgames.com>
2019-01-24 11:25:19 -0800

    added bake to brew install

--- profile/Base/install/brew_packages.sh
+++ profile/Base/install/brew_packages.sh
@@ -9,6 +9,12 @@ packages=(
   "openssh"
 )
 
+if ! brew tap | grep -q "github/kyleburton"; then
+  brew tap --full github/kyleburton https://github.com/kyleburton/homebrew-kyleburton.git
+else
+  echo "github/kyleburton already tapped"
+fi
+
 for p in "${packages[@]}"; do
   if ! brew ls --versions "$p" > /dev/null; then
     brew install "$p"
@@ -16,3 +22,10 @@ for p in "${packages[@]}"; do
     echo "package '$p' already installed!"
   fi
 done
+
+
+if ! brew ls --versions "github/kyleburton/bake" > /dev/null; then
+  brew install github/kyleburton/bake --without-bash
+else
+  echo "package 'github/kyleburton/bake' already installed!"
+fi
