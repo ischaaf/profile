cbcf6498a3a95a8ef16cd992caf5c9ab3e93ae70
Isaac Schaaf <zeekus99@gmail.com>
2019-01-26 00:46:28 -0800

    updated linuxbrew install

--- profile/Linux/source/linuxbrew.bash
+++ profile/Linux/source/linuxbrew.bash
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
 
-_add_path "$HOME/.linuxbrew/bin"
-_add_path "/home/linuxbrew/.linuxbrew/bin"
+_add_path "$(brew --prefix)/bin"
 
-if [ -f /home/ischaaf/.linuxbrew/share/bash-completion/bash_completion ]; then
-  . /home/ischaaf/.linuxbrew/share/bash-completion/bash_completion
-fi
-
-if [ -f /home/ischaaf/.linuxbrew/bin/bake-completion.sh ]; then
-  . /home/ischaaf/.linuxbrew/bin/bake-completion.sh
-fi
-
-if [ -f /home/linuxbrew/.linuxbrew/opt/bake/bin/bake-completion.sh ]; then
-  . /home/linuxbrew/.linuxbrew/opt/bake/bin/bake-completion.sh
-fi
-
-# Use Linuxbrew screen (TODO should be moved to Linux OS)
-if [ -f "$HOME/.linuxbrew/bin/screen" ]; then
-  alias screen="$HOME/.linuxbrew/bin/screen"
-fi
-
+[ -d "$(brew --prefix)/etc/bash_completion.d" ] && . "$(brew --prefix)/etc/bash_completion.d/"*
+[ -f "$(brew --prefix)/bin/opt/bake/bin/bake-completion.sh" ] \
+  && . "$(brew --prefix)/bin/opt/bake/bin/bake-completion.sh"
