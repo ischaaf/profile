e829e4e38f84e6ea138cfd13ad90cf53cf60fe26
Isaac Schaaf <ischaaf@riotgames.com>
2019-02-01 16:26:30 -0800

    removed Riot profile (moved to external extension

--- profile/Riot/install/search-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-CLI_PATH="$HOME/code/egh.riotgames.com/kda/search-cli/target/release/search-cli"
-
-# shellcheck disable=SC2015
-test -f "$CLI_PATH" && ln -s "$CLI_PATH" "$HOME/bin/search-cli" || :
