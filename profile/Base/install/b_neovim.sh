8ba77189e490762fd24714044f3f76a79803ae58
Isaac Schaaf <zeekus99@gmail.com>
2021-10-31 23:05:21 -0700

    added vim plug install

--- profile/Base/install/b_neovim.sh
+++ profile/Base/install/b_neovim.sh
@@ -6,3 +6,10 @@ if ! command -v nvim >/dev/null 2>&1; then
     sudo add-apt-repository ppa:neovim-ppa/unstable
     sudo apt-get install -y neovim
 fi
+
+NEOVIM_AUTOLOAD_FOLDER="$HOME/.local/share/nvim/site/autoload"
+PLUG_VIM="$NEOVIM_AUTOLOAD_FOLDER/plug.vim"
+
+if [ ! -f "$PLUG_VIM" ]; then
+  curl -fLo "$PLUG_VIM" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
