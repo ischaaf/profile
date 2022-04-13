01aff20971405153cde2cab2703efb71e85863b4
Isaac Schaaf <ischaaf@riotgames.com>
2022-04-13 13:02:24 -0700

    updated bake with bug fix, made golang install handle versions better

--- profile/Base/install/a_golang.sh
+++ profile/Base/install/a_golang.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 set -eu -o pipefail
+INSTALL_GO_VERSION="1.18"
 
 if [[ -d "/usr/local/go" ]]; then
-    exit 0
+    CURRENT_GO_VERSION="$(go version)"
+    if [[ "$CURRENT_GO_VERSION" == "$INSTALL_GO_VERSION" ]]; then
+        exit 0
+    fi
 fi
 
-wget https://golang.org/dl/go1.17.3.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz
-rm -f go1.17.3.linux-amd64.tar.gz
+wget "https://golang.org/dl/go${INSTALL_GO_VERSION}.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go${INSTALL_GO_VERSION}.linux-amd64.tar.gz"
+rm -f "go${INSTALL_GO_VERSION}.linux-amd64.tar.gz"
 
 (
     export GOROOT=/usr/local/go
     export GOPATH="$HOME/code"
     export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
-    GO111MODULE=on go get golang.org/x/tools/gopls@latest
+    go install golang.org/x/tools/gopls@latest
 )
