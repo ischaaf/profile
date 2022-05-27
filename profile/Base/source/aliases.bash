8796e55fb7582f11b1fb2ca23f702a432a42b8a6
Isaac Schaaf <zeekus99@gmail.com>
2022-05-27 15:20:23 -0700

    added convenience functions for tmux

--- profile/Base/source/aliases.bash
+++ profile/Base/source/aliases.bash
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 # Various Exports
 export RSYNC_RSH="ssh"
 export GREP_COLOR='1;32'
@@ -6,6 +6,17 @@ export GOPATH="$HOME/software/go"
 export PAGER="${PAGER:-less -RS}"
 export HISTCONTROL="ignoreboth"
 export EDITOR="nvim"
+
+function _tmux_session () {
+    if command -v tmux >/dev/null 2>&1; then
+        tmux display-message -p '#S' 2>/dev/null
+    fi
+}
+
+__tmux_session="$(_tmux_session)"
+if [[ -n "$TMUX_PANE" ]]; then
+    export HISTFILE="$HOME/.hists/${__tmux_session}_${TMUX_PANE}"
+fi
 
 # Aliases
 alias s='cd ..'
