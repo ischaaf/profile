28bc9d505c8ceac4ebbbe9fe8aab2281b1bcf543
Isaac Schaaf <ischaaf@riotgames.com>
2023-07-18 16:43:37 -0700

    added lua script configs for nvchad. this is a wip and still needs to get integrated into the profile

---
+++ scripts/custom/init.lua
@@ -0,0 +1,24 @@
+local g = vim.g
+local opt = vim.opt
+local autocmd = vim.api.nvim_create_autocmd
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+g.mapleader = ";"
+opt.clipboard = ""
+opt.guicursor = ""
+
+opt.backup = false
+opt.swapfile = false
+
+opt.scrolloff = 10
+opt.relativenumber = true
+opt.wrap = false
+
+autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end,
+})
---
+++ scripts/custom/plugins/init.lua
@@ -0,0 +1,54 @@
+local override = require "custom.plugins.configs.override"
+
+return {
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      ---@diagnostic disable-next-line: different-requires
+      require "custom.plugins.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.configs.null-ls"
+    end,
+  },
+  { "williamboman/mason.nvim", override_options = override.mason },
+
+  { "christoomey/vim-tmux-navigator", lazy = false },
+
+  -- UI
+  { "goolord/alpha-nvim", disable = false },
+  { "folke/which-key.nvim", disable = false },
+  { "NvChad/ui", override_options = override.ui },
+
+  -- Git
+  { "lewis6991/gitsigns.nvim", override_options = override.gitsigns },
+
+  -- Treesitter
+  { "nvim-treesitter/nvim-treesitter", override_options = override.treesitter },
+  { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    after = "nvim-treesitter",
+    config = function()
+      require "custom.plugins.configs.treesitter-context"
+    end,
+  },
+
+  -- Editor
+  {
+    "tpope/vim-surround",
+    opt = true,
+    event = "BufReadPost",
+  },
+  {
+    "hrsh7th/vim-eft",
+    opt = true,
+    event = "BufReadPost",
+  },
+}
