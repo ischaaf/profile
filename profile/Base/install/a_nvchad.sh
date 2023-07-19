259327919f88acaadbc33e8b170aebbc7ce005cc
Isaac Schaaf <zeekus99@gmail.com>
2023-07-18 19:03:49 -0700

    moved nvchad configs into profile folder. Updated reinstall script to properly cleanup files linked into the home directory

---
+++ profile/Base/install/a_nvchad.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eEuo pipefail
+
+if [[ -d "$HOME/.config/nvim/lua/.git" ]]; then
+  # already installed
+  exit 0
+fi
+
+(
+  # clean out cached files as these will break nvchad setup
+  rm -rf "$HOME/.cache/nvim" "$HOME/.local/share/nvim"
+
+  cd "$HOME/.config/nvim/"
+  git init
+  git remote add origin git@github.com:NvChad/NvChad.git
+  git pull origin v2.0
+  git fetch
+  git checkout -b v2.0
+  git pob
+)
