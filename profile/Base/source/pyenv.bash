dd5d65f4229076d74423531a6521c56e2495ebea
Isaac Schaaf <ischaaf@riotgames.com>
2024-08-27 09:06:12 -0700

    updated go version, changed named-tmux defaults, fixed bake issues, flipped pyenv on again

--- profile/Base/source/pyenv.bash
+++ profile/Base/source/pyenv.bash
@@ -7,8 +7,8 @@ if ! command -v pyenv >/dev/null 2>&1; then
     return
 fi
 
-# export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-# export PYTHONSTARTUP="$HOME/.pythonrc.py"
-# eval "$(pyenv init -)"
-# eval "$(pyenv init --path)"
-# eval "$(pyenv virtualenv-init -)"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export PYTHONSTARTUP="$HOME/.pythonrc.py"
+eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
