f5f2a66d573cb6803c4250324233118bb5d71a4a
Isaac Schaaf <zeekus99@gmail.com>
2025-06-25 12:15:19 -0700

    added neovide config

--- profile/Base/home/.config/nvim/lua/config/options.lua
+++ profile/Base/home/.config/nvim/lua/config/options.lua
@@ -73,3 +73,9 @@ local function set_tab(opts)
 end
 
 vim.api.nvim_create_user_command("SetTab", set_tab, { nargs = "?" })
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
+  vim.g.neovide_cursor_animation_length = 0
+end
