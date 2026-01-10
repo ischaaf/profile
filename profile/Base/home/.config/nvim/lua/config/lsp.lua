ef88d8779895a3be548e15e6a8d1f876125e7400
Isaac Schaaf <ischaaf@riotgames.com>
2026-01-09 16:34:52 -0800

    small fixes

--- profile/Base/home/.config/nvim/lua/config/lsp.lua
+++ profile/Base/home/.config/nvim/lua/config/lsp.lua
@@ -123,25 +123,27 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
     -- vim.keymap.del("n", "<leader>dv")
     keymap("n", "<leader>d", lsp.buf.definition, opt("Go to definition"))
-    keymap("n", "<leader>i", function() lsp.buf.implementation({ border = "single" })  end, opt("Go to implementation"))
+    keymap("n", "<leader>i", function() lsp.buf.implementation({ border = "single" }) end, opt("Go to implementation"))
     -- keymap("n", "<leader>r", lsp.buf.references, opt("Show References"))
     keymap("n", "cd", vim.diagnostic.open_float, opt("Open diagnostic in float"))
     keymap("n", "<C-k>", lsp.buf.signature_help, opts)
     -- disable the default binding first before using a custom one
     pcall(vim.keymap.del, "n", "K", { buffer = ev.buf })
-    keymap("n", "K", function() lsp.buf.hover({ border = "single", max_height = 30, max_width = 120 }) end, opt("Toggle hover"))
+    keymap("n", "K", function() lsp.buf.hover({ border = "single", max_height = 30, max_width = 120 }) end,
+      opt("Toggle hover"))
     keymap("n", "<Leader>lF", vim.cmd.FormatToggle, opt("Toggle AutoFormat"))
     keymap("n", "<Leader>lI", vim.cmd.Mason, opt("Mason"))
     keymap("n", "<Leader>lS", lsp.buf.workspace_symbol, opt("Workspace Symbols"))
     keymap("n", "<Leader>la", lsp.buf.code_action, opt("Code Action"))
-    keymap("n", "<Leader>lh", function() lsp.inlay_hint.enable(not lsp.inlay_hint.is_enabled({})) end, opt("Toggle Inlayhints"))
+    keymap("n", "<Leader>lh", function() lsp.inlay_hint.enable(not lsp.inlay_hint.is_enabled({})) end,
+      opt("Toggle Inlayhints"))
     keymap("n", "<Leader>li", vim.cmd.LspInfo, opt("LspInfo"))
     keymap("n", "<Leader>ll", lsp.codelens.run, opt("Run CodeLens"))
     keymap("n", "<Leader>lr", lsp.buf.rename, opt("Rename"))
     keymap("n", "<Leader>ls", lsp.buf.document_symbol, opt("Doument Symbols"))
 
     keymap("n", "<Leader>e", function() vim.diagnostic.jump({ count = 1, float = true }) end, opt("Next Diagnostic"))
-    keymap("n", "<Leader>E", function() vim.diagnostic.jump({ count =-1, float = true }) end, opt("Prev Diagnostic"))
+    keymap("n", "<Leader>E", function() vim.diagnostic.jump({ count = -1, float = true }) end, opt("Prev Diagnostic"))
     -- stylua: ignore end
   end,
 })
@@ -223,7 +225,7 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.uv.cwd(),
     })
     local client =
-      vim.lsp.start(vim.tbl_extend("force", vim.lsp.config.basedpyright, { root_dir = root }), { attach = false })
+        vim.lsp.start(vim.tbl_extend("force", vim.lsp.config.basedpyright, { root_dir = root }), { attach = false })
     if client then
       vim.lsp.buf_attach_client(0, client)
     end
