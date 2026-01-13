ece756ac6213733f08ab21a7ad3aecfb4b4b17bc
Isaac Schaaf <zeekus99@gmail.com>
2026-01-12 16:33:53 -0800

    updated nodenv to work with the latest version

--- profile/Base/install/a_nodenv.sh
+++ profile/Base/install/a_nodenv.sh
@@ -8,10 +8,6 @@ if [[ ! -d "$PROFILE_TOOL_DIR/nodenv" ]]; then
     git clone https://github.com/nodenv/nodenv.git "$PROFILE_TOOL_DIR/nodenv"
 fi
 
-(
-    cd "$PROFILE_TOOL_DIR/nodenv" && src/configure && make -C src
-)
-
 if [[ ! -f "$HOME/bin/nodenv" ]]; then
     ln -s "$PROFILE_TOOL_DIR/nodenv/bin/nodenv" "$HOME/bin/nodenv"
 fi
